c348cb21ecdc54eda855d06e0d6cfcffe9b33da2
Moises Veliz <msveliz16@gmail.com>
2020-04-06 05:13:04 -0400

    add the ingnore file

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@
 	const contenedor = document.querySelector('#container');
 	const btnBars = document.querySelector('#btn-bars');
 	const screen = window;
+	const interruptor = document.querySelector('#switch');
+	const header = document.querySelector('#header');
+	const menu = document.querySelector('#menu');
+	const main = document.querySelector('#main');
 
 	// Funciones
 	const medirAncho = function () {
@@ -13,11 +17,18 @@
 	medirAncho();
 
 	// Eventos
-	btnBars.addEventListener('click', () => {
+	btnBars.addEventListener( 'click', () => {
 		contenedor.classList.toggle('desplegar');
 	} );
 
-	screen.addEventListener('resize', () => {
+	screen.addEventListener( 'resize', () => {
 		medirAncho();
+	} );
+
+	interruptor.addEventListener( 'click', () => {
+		header.classList.toggle('dark');
+		menu.classList.toggle('dark');
+		main.classList.toggle('dark');
+		interruptor.classList.toggle('activate');
 	} );
 })();
